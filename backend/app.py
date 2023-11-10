ade23ef21dcdaff320a4fbcd0a35e6009cc4f1d6
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-11-10 16:44:30 +0100

    backend integration with frontend test successfull

--- backend/app.py
+++ backend/app.py
@@ -8,5 +8,10 @@ CORS(app)
 def index():
     return jsonify({"message": "Welcome to the AI Bank Project API!"})
 
+@app.route('/test')
+def test():
+    return jsonify({"message": "Test successful!"})
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [backendResponse, setBackendResponse] = useState('');
+
+  useEffect(() => {
+    fetch('/test')
+      .then(response => response.json())
+      .then(data => setBackendResponse(data.message))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Response from backend: {backendResponse}</p>
       </header>
     </div>
   );
