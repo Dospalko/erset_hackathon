0d1e41ea7e41bb2a78a8fe0ef63f60c8462e7b59
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-11-10 17:09:00 +0100

    feat: calculating expension and analyzing it

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 from flask_cors import CORS
+from data_analysis import load_transactions, calculate_repayment_plan, load_loan_details
 
 app = Flask(__name__)
 CORS(app)
@@ -8,10 +9,17 @@ CORS(app)
 def index():
     return jsonify({"message": "Welcome to the AI Bank Project API!"})
 
-@app.route('/test')
-def test():
-    return jsonify({"message": "Test successful!"})
+@app.route('/analyze', methods=['GET'])
+def analyze():
+    result = load_transactions()
+    return jsonify(result)
 
+@app.route('/calculate', methods=['GET'])
+def calculate():
+    monthly_expenses = load_transactions()
+    loan_details = load_loan_details()
+    result = calculate_repayment_plan(monthly_expenses, loan_details)
+    return jsonify(result)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/data_analysis.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import json
+from datetime import datetime
+
+# Cesty k vašim JSON súborom
+TRANSACTIONS_FILE_PATH = 'data/transactions.json'
+LOAN_DETAILS_FILE_PATH = 'data/loan_details.json'
+
+def load_transactions(file_path=TRANSACTIONS_FILE_PATH):
+    with open(file_path, 'r') as file:
+        transactions = json.load(file)
+    return analyze_expenses(transactions)
+
+def analyze_expenses(transactions):
+    df = pd.DataFrame(transactions)
+    df['date'] = pd.to_datetime(df['date'])
+    df.set_index('date', inplace=True)
+
+    # Výpočet mesačných výdavkov
+    monthly_expenses = df.resample('M').sum()
+
+    # Konvertovanie kľúčov na reťazce vo formáte 'YYYY-MM'
+    return {date.strftime('%Y-%m'): amount for date, amount in monthly_expenses['amount'].items()}
+
+def load_loan_details(file_path=LOAN_DETAILS_FILE_PATH):
+    with open(file_path, 'r') as file:
+        loan_details = json.load(file)
+    return loan_details
+
+def calculate_repayment_plan(monthly_expenses, loan_details):
+    loan_amount = loan_details['loan_amount']
+    annual_interest_rate = loan_details['annual_interest_rate']
+    loan_term_years = loan_details['loan_term_years']
+
+    monthly_rate = annual_interest_rate / 12 / 100
+    total_payments = loan_term_years * 12
+    basic_monthly_payment = loan_amount * monthly_rate / (1 - (1 + monthly_rate) ** -total_payments)
+
+    max_expense = max(monthly_expenses.values())
+    adjusted_payments = {month.strftime('%Y-%m'): basic_monthly_payment - (expense / max_expense * basic_monthly_payment * 0.25)
+                         for month, expense in monthly_expenses.items()}
+
+    return adjusted_payments
